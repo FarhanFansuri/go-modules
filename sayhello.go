728d4c891afae0558fb92a7dbbba7d26d1b381dd
MFarhanFansuri <farhanfansurigithub@gamil.com>
2024-11-06 07:15:33 +0700

    ubah file

--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,5 @@
 package sayhello // Ubah menjadi huruf kecil dan tidak menggunakan garis bawah
 
 func SayHello() string {
-	return "Say Hello" // Pastikan penggunaan string benar
+	return "Say Hello World" // Pastikan penggunaan string benar
 }
